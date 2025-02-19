96822ab28d7d1e1502a7144de6cb9a1cc0051ecb
shashank-padala <shashank.padala24@gmail.com>
2025-02-19 16:14:29 -0500

    deleted ui loader component as it's not used

--- components/ui/Loader.tsx
+++
@@ -1,7 +0,0 @@
-export function Loader() {
-  return (
-    <div className="flex justify-center items-center h-20">
-      <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-blue-600"></div>
-    </div>
-  );
-}
