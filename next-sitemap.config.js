efa3a7bbeaca267857d87fbe67a58306534e4976
shashank-padala <shashank.padala24@gmail.com>
2025-04-16 23:12:23 -0400

    updated sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,15 +7,5 @@ module.exports = {
   pagesDirectory: 'app',
   sourceDir:    'app',
   exclude: ['/my-profile', '/exams/**'],
-  robotsTxtOptions: { /*…*/ },
-
-  // manually inject all your dynamic “tips” URLs
-  additionalPaths: async (config) => {
-    // e.g. read your local data or query your CMS
-    const slugs = await fetchTipSlugs();
-    return slugs.map((slug) => ({
-      loc: `/ielts-exam-tips/${slug}`,
-      lastmod: new Date().toISOString(),
-    }));
-  },
+  robotsTxtOptions: { /*…*/ }
 }
